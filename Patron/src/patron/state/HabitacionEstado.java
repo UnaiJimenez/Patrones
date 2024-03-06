96edb3334898d0c5564a7b255286bc94732a6bb7
Unai Jimenez <ikcto@plaiaundi.net>
2024-03-06 09:28:21 +0100

    commit

---
+++ Patron/src/patron/state/HabitacionEstado.java
@@ -0,0 +1,9 @@
+package patron.state;
+
+public interface HabitacionEstado {
+	public HabitacionEstado ocupar();
+	public HabitacionEstado liberar();
+	public HabitacionEstado limpiar();
+	public HabitacionEstado salir();
+
+}
